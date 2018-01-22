e8c87d9367c051f25fb41e2fae3563a8561d29db
Karen Tercero <krntrcr@gmail.com>
2018-01-22 14:09:45 -0800

    search, list, map are working, need to fix layout

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-//geocode address and retrieve information from iNaturalist API
-  // 1) initialize map first
-  // 2) create function to geocode address
-  // 3) create function to get data from API
-  // 4) create function to create a list
-    // a) create object which contains species as keys and observations as values
-  // 5) create callback function to display the data from API
-  // 6) create function that watches when we click the submit button
-  // 7) create an array of markers which consists of observations of a given species
-  // 8) create function that  displays markers on map 
-
-
 // 1) initialize map
 
 var geocoder;
@@ -20,7 +8,6 @@ function initMap() {
     geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: {lat: -34.397, lng: 150.644}
   });
 }
 
@@ -28,24 +15,20 @@ function initMap() {
 
 let coordinates
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
+function geocodeAddress(callback) {
+  const queryTarget = $(".js-search-form .js-query");
+  let address = queryTarget.val();
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       coordinates = results[0].geometry.location;
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location,
-        title: "Location search"
-      });
+      map.setCenter(results[0].geometry.location);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
     coordinates = JSON.parse(JSON.stringify(results[0].geometry.location));
-    getDataFromApi(coordinates, displayObservationData);
-    console.log(coordinates, "search location")
+    getDataFromApi(coordinates, callback);
   });
+  queryTarget.val("");
 }
 
 // 3) create function to get data from API
@@ -83,15 +66,35 @@ function sortObservations(data) {
 
 function renderSpecies(speciesName) {
     if (speciesName == "null") return;
-    return `<li class="name-on-list"><img src="" alt=""><span class="species">${speciesName}</span></li>`;
+    return `<li class="name-on-list"><span class="species">${speciesName}</span></li>`;
+  }
+
+const markers = [];
+
+function showMarkers(speciesName) {
+  const speciesArray = species[speciesName];
+  if (!speciesName||!speciesName.length) return;
+  speciesArray.forEach(function(observation) {
+    const latCoordinate = parseFloat(observation.geojson.coordinates[1]);
+    const lngCoordinate = parseFloat(observation.geojson.coordinates[0]);
+    const coordinates = {lat: latCoordinate, lng: lngCoordinate};     
+    let marker = new google.maps.Marker({
+      map: map,
+      position: coordinates,
+      title: observation.species_guess,
+    });
+    markers.push(marker);
+  });    
 }
 
+//////////////////////////////
 // 5) create callback function to display the data from API
 
 function displayObservationData(data) {
   sortObservations(data.results);
-  // console.log(JSON.stringify(data));
   var names = Object.keys(species).map(renderSpecies);
+  Object.keys(species).forEach(showMarkers);  
+  filterMarkers();
   $(".js-search-results").html(`<ul class="species">${names.join("")}</ul>`);
 }
 
@@ -101,48 +104,16 @@ function watchSubmit() {
   $(".js-search-form").submit(event => {
     event.preventDefault();
     $("#home-screen").hide();
-    $("#list-screen").show();
-    geocodeAddress(geocoder, map);
-    const queryTarget = $(event.currentTarget).find(`.js-query`);
-    queryTarget.val("");
-  });
+    $("#list").show();
+    $("#description-container").hide();
+    $("#map").show();
+    geocodeAddress(displayObservationData);
+});
 }
 
 $(watchSubmit);
 
-// 7) create an array of markers which consists of observations of a given species
-
-// let markers
-
-// function getCoordinates()
-
-// function 
-//   var marker = new google.maps.Marker({
-//   position: 
-//   })
-function showMarkers(speciesName) {
-  const speciesArray = species[speciesName];
-  if (!speciesName||!speciesName.length) return;
-  let minLat = 180.0, maxLat = -180.0, minLng = 180.0, maxLng = -180.0;
-  speciesArray.forEach(function(observation) {
-    const latCoordinate = parseFloat(observation.geojson.coordinates[1]);
-    const lngCoordinate = parseFloat(observation.geojson.coordinates[0]);
-    if (maxLat < latCoordinate) maxLat = latCoordinate;
-    if (minLat > latCoordinate) minLat = latCoordinate;
-    if (maxLng < lngCoordinate) maxLng = lngCoordinate;
-    if (minLng > lngCoordinate) minLng = lngCoordinate;
-    const coordinates = {lat: latCoordinate, lng: lngCoordinate};
-    console.log("maxLat", maxLat, "minLat", minLat, "maxLng", maxLng, "minLng", minLng, "coordinates", coordinates);
-    var marker = new google.maps.Marker({
-      map: map,
-      position: coordinates,
-      title: observation.place_guess
-    });
-  });
-  var mapcoordinates = {lat: minLat + (maxLat-minLat)/2, lng: minLng + (maxLng-minLng)/2};
-  map.setCenter(mapcoordinates)
-  console.log(mapcoordinates)
-}
+//functions that access the taxonomy for each species
 
 function nameOrder(observation) {
   let order = observation.identifications[0].taxon.ancestors[4].name;
@@ -165,22 +136,6 @@ function nameGenus(observation) {
   return genus + " (" + observation.identifications[0].taxon.ancestors[6].preferred_common_name + ")";
 }
 
-// function addComment(observation) {
-//   let comment;
-//   if (observation.comments.length === 0) return "N/A";
-//   for (i = 0; i < observation.comments.length; i++) {
-//     let comment = 
-//       `<li class="comment">
-//         <p class="comment-body">${observation.comments[i].body}<p>
-//         <img class="user-icon" src=${observation.comments[i].user.icon}>
-//         <p class="username">${observation.comments[i].user.login}</p>
-//         <p class="date">${observation.comments[i].created_at_details.date}</p>
-//       </li>`;
-//   $("#comments").html(comment);
-//   };
-//   console.log(observation.comments);
-// }
-
 function addDescription(observation) {
   let description = 
     `<section id="pd-container">
@@ -197,31 +152,25 @@ function addDescription(observation) {
       </section>
     </section>`;
  $("#description-container").html(description);
-  console.log(observation.identifications);
 }
 
 $(function() {
-  $("#list-screen").hide();
-  $("#bird-and-map").hide();
   $("ul").on("click", "li", function(event) {
-    $("#list-screen").hide();
-    $("#bird-and-map").show();
+    $("#list").hide();
+    $("#description-container").show();
     const speciesName = $(this).find(".species").text();
     species[speciesName].forEach(addDescription);
+    filterMarkers(speciesName);
     google.maps.event.trigger(map, 'resize');
-    showMarkers(speciesName);
     getSpeciesDescriptionHTML(speciesName,renderSpeciesDescription);
   });
 });
 
 function goBack() {
-  $("#bird-and-map").hide();
-  $("#list-screen").show();
+  filterMarkers();
+  $("#description-container").hide();
+  $("#list").show();
 }
-
-// function findHTMLpage (speciesName) {
-//   let page = "http://"
-// }
 
 function renderSpeciesDescription (data) {
   let $data = $(data);
@@ -230,12 +179,40 @@ function renderSpeciesDescription (data) {
 }
 
 function getSpeciesDescriptionHTML (speciesName, callback) {
-  speciesName = speciesName.replace(/\s+/g, '-')
-  .replace(/[',"]+/g, '').toLowerCase();
-  const url = "http://www.audubon.org/field-guide/bird/" + speciesName;
-  $.get(url, callback)
+  let speciesNameUrl = speciesName.replace(/\s+/g, '-').replace(/[',"]+/g, '').toLowerCase();
+  const audubonUrl = "http://www.audubon.org/field-guide/bird/" + speciesNameUrl;
+  var url = "http://anyorigin.com/go?url=" + encodeURIComponent(audubonUrl) + "&callback=?";
+  $.get(url, callback);
 }
 
-// #node-926 > section > div > div.large-8.columns > section:nth-child(1) > div.hide-for-tiny.hide-for-small.hide-for-medium
+//show markers function only gets called once at the same time list of species
+//gets built and remove species specific code from showmarkers so it always shows 
+//all species
+//need new function filtermarkers forEach markers (gets built by showmarkers)
+//code thats about building lat and lng should be in filtermarkers or another
+// function called centerOnVisibleMarkers
+//showmarkers should call filter markers, calls centeronvisiblemarkers
+// goBack should call filtermarkers()
+//handler for click on speciesname should call filtermarkers(speciesName)
+//showmarkers needs to set species name on marker
 
-// #node-649 > section > div > div.large-8.columns > section:nth-child(1) > div.hide-for-tiny.hide-for-small.hide-for-medium
+function filterMarkers(speciesName) {
+  let minLat = 180.0, maxLat = -180.0, minLng = 180.0, maxLng = -180.0;
+  for (i=0; i < markers.length; i++) {
+    let marker = markers[i];
+    let latCoordinate = marker.position.lat();
+    let lngCoordinate = marker.position.lng();
+    if (marker.title == speciesName || !speciesName) {
+      if (maxLat < latCoordinate) maxLat = latCoordinate;
+      if (minLat > latCoordinate) minLat = latCoordinate;
+      if (maxLng < lngCoordinate) maxLng = lngCoordinate;
+      if (minLng > lngCoordinate) minLng = lngCoordinate;
+      marker.setVisible(true)
+    }
+    else marker.setVisible(false);
+  }
+  var mapcoordinates = {lat: minLat + (maxLat-minLat)/2, lng: minLng + (maxLng-minLng)/2};
+  if (!speciesName) map.setCenter(mapcoordinates);
+  console.log(mapcoordinates);
+  google.maps.event.trigger(map, 'resize');  
+} 
